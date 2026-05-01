e4efc0c3d1fab835a9e1f29f284ffaa0d524c74d
sotomayornowire-tech <sotomayor.nowire@gmail.com>
2026-05-01 14:29:02 -0300

    Delete commands/general/kripton.js

--- commands/general/kripton.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('kripton')
-        .setDescription('Muestra el estado del addon'),
-    async execute(interaction) {
-        await interaction.reply('🟩 **Kripton Addon** está activo y funcionando.');
-    },
-};

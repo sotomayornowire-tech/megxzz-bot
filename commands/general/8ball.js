88d31bd516d747ffed6c63bcc4e06fc78bc309b0
sotomayornowire-tech <sotomayor.nowire@gmail.com>
2026-05-01 15:14:04 -0300

    Update 8ball.js

--- commands/general/8ball.js
+++ commands/general/8ball.js
@@ -3,9 +3,8 @@ module.exports = {
     data: new SlashCommandBuilder().setName('info').setDescription('Info de Kripton Addon'),
     async execute(interaction) {
         const embed = new EmbedBuilder()
-            .setTitle('🟩 Kripton Addon')
+            .setTitle('🟩 Megxzz activo')
             .setColor(0x00FF00)
-            .setDescription('El mejor addon para Meteor Client.')
             .addFields({ name: 'Estado', value: 'Operativo', inline: true });
         await interaction.reply({ embeds: [embed] });
     },
